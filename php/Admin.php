03fc9e45ba34eb816e89db1a53d0e8917461c408
lorusak <loup.rusak@etu.uca.fr>
2020-11-29 11:17:29 +0100

    Modification des vues et création des classes métiers du blog

---
+++ php/Admin.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Admin extends User
+{
+    private $login;
+
+    private $password;
+
+    public function deleteNews()
+    {
+
+    }
+}
---
+++ php/User.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class User extends Visitor
+{
+    private $login;
+
+    private $password;
+
+    public function addNews(){
+
+    }
+
+    public function deleteNews(){
+
+    }
+
+    public function connection(){
+
+    }
+}
