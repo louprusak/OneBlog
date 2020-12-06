e61644efad70dd570a1e3bacfa343e5155241eef
malevyvale <manoah.levy_valensi@etu.uca.fr>
2020-12-06 14:03:00 +0100

    réarrangement des repertoires

--- views/connection.php
+++ views/connection.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Connexion</title>
-    <link href="../css/bootstrap.css" rel="stylesheet"/>
-    <link href="../css/style.css" rel="stylesheet"/>
+    <link href="css/bootstrap.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet"/>
     <script src="js/jquery.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/scroll-animate.js"></script>
 </head>
 
