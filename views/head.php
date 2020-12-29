65bcfefbd8446f64559bcae6e6bfd7c5c1ffaab0
lorusak <loup.rusak@etu.uca.fr>
2020-12-29 19:58:08 +0100

    modif head

--- views/head.php
+++ views/head.php
@@ -1,8 +1,8 @@
 
-<link href="/css/bootstrap.css" rel="stylesheet"/>
-<link href="/css/style.css" rel="stylesheet"/>
-<script src="/js/jquery.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-<script src="/js/scroll-animate.js"></script>
+<link href="<?=__DIR__?>/../css/bootstrap.css" rel="stylesheet"/>
+<link href="<?=__DIR__?>/../css/style.css" rel="stylesheet"/>
+<script src="<?=__DIR__?>/../js/jquery.js"></script>
+<script src="<?=__DIR__?>/../js/bootstrap.min.js"></script>
+<script src="<?=__DIR__?>/../js/scroll-animate.js"></script>
 
 
