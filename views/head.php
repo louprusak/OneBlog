e9e7aeb749b8d6bfa95f3bf9a831bc028f0b6282
lorusak <loup.rusak@etu.uca.fr>
2020-12-29 19:43:48 +0100

    Résolution problème css

--- views/head.php
+++ views/head.php
@@ -1,8 +1,8 @@
 
-<link href="css/bootstrap.css" rel="stylesheet"/>
-<link href="<?php __DIR__?>../css/style.css" rel="stylesheet"/>
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/scroll-animate.js"></script>
+<link href="/css/bootstrap.css" rel="stylesheet"/>
+<link href="/css/style.css" rel="stylesheet"/>
+<script src="/js/jquery.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/scroll-animate.js"></script>
 
 
